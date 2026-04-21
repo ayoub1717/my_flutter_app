d8b11b7042bc1dcbb183e49bca780d78578dda59
Ego-Prodection <bargaouiayoub1717@gmail.com>
2026-04-22 00:13:31 +0100

    update project

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'home_page.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart'; // 👈 ضرورية للحفظ
+import 'dart:convert'; // 👈 ضرورية لفك شفرة البيانات
 import 'login_page.dart';
+import 'home_page.dart'; // 👈 تأكد من استيراد صفحة الـ Home
 import 'firebase_options.dart';
 
-// 🔔 (اختياري) messaging
+// 🔔 messaging
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // 1. تشغيل Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  // 🔥 نشغّل messaging كان في Android / iOS
+  // 2. تشغيل Messaging للـ Mobile
   if (!kIsWeb) {
     await initMessaging();
   }
 
-  runApp(const MyApp());
+  // 3. التثبت من وجود مستخدم مسجل دخول سابقاً
+  final prefs = await SharedPreferences.getInstance();
+  final String? userRawData = prefs.getString('user_data');
+
+  Widget initialScreen;
+
+  if (userRawData != null) {
+    // إذا لقينا بيانات، نبعثوها للـ Home مباشرة
+    initialScreen = HomePage(userData: jsonDecode(userRawData));
+  } else {
+    // إذا مالقيناش، نفتحو صفحة الـ Login
+    initialScreen = const LoginPage();
+  }
+
+  runApp(MyApp(firstScreen: initialScreen));
 }
 
-// 🔔 Firebase Messaging setup (mobile فقط)
+// 🔔 Firebase Messaging setup
 Future<void> initMessaging() async {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-  // طلب permission
-  NotificationSettings settings = await messaging.requestPermission();
-
-  print('Permission: ${settings.authorizationStatus}');
-
-  // token
+  await messaging.requestPermission();
   String? token = await messaging.getToken();
   print("FCM Token: $token");
 
-  // استقبال notification
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
     print("New message: ${message.notification?.title}");
   });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Widget firstScreen;
+  const MyApp({super.key, required this.firstScreen});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Bargou Gym',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      home: const LoginPage(),
+      theme: ThemeData.dark(), // استعملنا الـ Dark كـ افتراضي
+      home: firstScreen, // يفتح الصفحة اللي قررها الـ main
     );
   }
 }
